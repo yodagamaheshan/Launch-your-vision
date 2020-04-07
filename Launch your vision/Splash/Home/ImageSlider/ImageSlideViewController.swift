4b17535ed68118367c53d1947bb35bcaf31a5fdd
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-04-07 12:23:50 +0530

    image slide

--- Launch your vision/Splash/Home/ImageSlider/ImageSlideViewController.swift
+++ Launch your vision/Splash/Home/ImageSlider/ImageSlideViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class ImageSlideViewController: UIViewController {
     
+    @IBOutlet weak var imageSlideShow: ImageSlideshow!
     
+     //TODO: change with real data
+    let localSource = [BundleImageSource(imageString: "img1"), BundleImageSource(imageString: "img2"), BundleImageSource(imageString: "img3")]
+
     static func make() -> WebViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: String(describing: WebViewController.self)) as? WebViewController
@@ -19,8 +24,22 @@ class ImageSlideViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    func setupViews(){
+        imageSlideShow.circular = false
+        imageSlideShow.pageIndicatorPosition = .init(horizontal: .center, vertical: .bottom)
+               imageSlideShow.contentScaleMode = UIViewContentMode.scaleAspectFill
         
-        // Do any additional setup after loading the view.
+        let pageControl = UIPageControl()
+         //TODO: change with app color
+        pageControl.currentPageIndicatorTintColor = UIColor(named: "selectedColoe")
+        pageControl.pageIndicatorTintColor = UIColor(named: "NormalColor")
+        imageSlideShow.pageIndicator = pageControl
+        
+        imageSlideShow.activityIndicator = DefaultActivityIndicator()
+        imageSlideShow.setImageInputs(localSource)
     }
     
 }
