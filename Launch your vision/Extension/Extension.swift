1603bef9a5c3d7bf58c4f41d3d12b78ca56eab75
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-04-10 06:55:14 +0530

    app color

---
+++ Launch your vision/Extension/Extension.swift
@@ -0,0 +1,26 @@
+//
+//  Extension.swift
+//  Launch your vision
+//
+//  Created by Heshan Yodagama on 4/10/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    enum AppColor: String{
+        case button1 = " butonColor-1"
+        case button2 = "butonColor-2"
+        case button3 = "butonColor-3"
+        case button4 = "butonColor-4"
+        case button5 = "butonColor-5"
+        case button6 = " butonColor-6"
+        case mainPink = "mainn-pink"
+    }
+    
+    func getAppColor(color: AppColor) -> UIColor{
+        return UIColor(named: color.rawValue)!
+    }
+}
