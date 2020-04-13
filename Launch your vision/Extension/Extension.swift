ac17917c4897b6d13c14af5250f733addffa2900
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-04-13 21:28:51 +0530

    add colors to extension

--- Launch your vision/Extension/Extension.swift
+++ Launch your vision/Extension/Extension.swift
@@ -11,13 +11,17 @@ import UIKit
 
 extension UIColor{
     enum AppColor: String{
-        case button1 = " butonColor-1"
+        case button1 = "butonColor-1"
         case button2 = "butonColor-2"
         case button3 = "butonColor-3"
         case button4 = "butonColor-4"
         case button5 = "butonColor-5"
-        case button6 = " butonColor-6"
+        case button6 = "butonColor-6"
         case mainPink = "mainn-pink"
+        case sub1 = "subtitle color-1"
+        case sub2 = "subtitle color-2"
+        case sub3 = "subtitle color-3"
+        case sub4 = "subtitle color-4"
     }
     
     static func getAppColor(color: AppColor) -> UIColor{
